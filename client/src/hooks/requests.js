72b1f48f13706e1f959fd98ad631a965c45afa80
DSonWork2501 <sondanhwork2501@gmail.com>
2023-10-29 13:09:56 +0700

    first push

---
+++ client/src/hooks/requests.js
@@ -0,0 +1,51 @@
+const API_URL = 'http://localhost:8000';
+
+async function httpGetPlanets() {
+  const response = await fetch(`${API_URL}/planets`);
+  return await response.json();
+}
+
+async function httpGetLaunches() {
+  const response = await fetch(`${API_URL}/launches`);
+  const fetchedLauches = await response.json();
+  return fetchedLauches.sort((a, b) => {
+    return a.flightNumber - b.flightNumber;
+  });
+}
+
+async function httpSubmitLaunch(launch) {
+  try {
+    return await fetch(`${API_URL}/launches`, {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(launch),
+    });
+  } catch (error) {
+    return {
+      ok: false,
+    };
+  }
+}
+
+async function httpAbortLaunch(id) {
+  try {
+    return await fetch(`${API_URL}/launches/${id}`, {
+      method: "delete",
+    });
+  } catch (error) {
+    console.log(error)
+    return {
+      ok: false,
+    };
+  }
+
+}
+
+export {
+  httpGetPlanets,
+  httpGetLaunches,
+  httpSubmitLaunch,
+  httpAbortLaunch,
+};
